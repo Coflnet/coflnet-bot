e504b4473ecc629acb1b7190fed3714ad5a8b4ac
Flou21 <muehlhans.florian@web.de>
2022-05-14 21:29:21 +0200

    add null check

--- internal/discord/role.go
+++ internal/discord/role.go
@@ -8,9 +8,15 @@ import (
 )
 
 func SetFlipperRoleForUser(user *mongo.User) error {
+
+	if len(user.DiscordNames) == 0 {
+		log.Info().Msgf("user %d has no discord names, skip flipper role check", user.UserId)
+		return nil
+	}
+
 	if user.DiscordNames[0] == "" {
 
-		log.Warn().Msgf("user %s has no discord name, skip flip role set", user.UserId)
+		log.Info().Msgf("user %s has no discord name, skip flip role set", user.UserId)
 		return nil
 	}
 
