b5a8f44aead77649c76dc5403bb428d2e940bbc6
Florian Mühlhans <muehlhans.florian@web.de>
2022-08-24 19:16:59 +0200

    add a time sleep

--- internal/usecase/mc_verify.go
+++ internal/usecase/mc_verify.go
@@ -9,6 +9,7 @@ import (
 	kafkago "github.com/segmentio/kafka-go"
 	"github.com/vmihailenco/msgpack/v5"
 	"strconv"
+	"time"
 )
 
 // when a user verifies their account, maybe he is qualified for the flipper role
@@ -36,6 +37,8 @@ func StartMcVerifyConsumer() {
 			log.Error().Err(err).Msg("failed to commit mc_verify topic")
 			continue
 		}
+
+		time.Sleep(time.Minute * 5)
 	}
 }
 
