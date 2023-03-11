f8eae5559b8bce76a83eb9d836227e435d51ff8b
Florian Mühlhans <muehlhans.florian@web.de>
2023-03-11 22:27:35 +0100

    add some logs when registering and unregestering commands

--- internal/discord/commands.go
+++ internal/discord/commands.go
@@ -9,26 +9,24 @@ import (
 )
 
 type DiscordCommand interface {
-    Name() string
-    CreateCommand() *discordgo.ApplicationCommand
-    HandleCommand(s *discordgo.Session, i *discordgo.InteractionCreate)
+	Name() string
+	CreateCommand() *discordgo.ApplicationCommand
+	HandleCommand(s *discordgo.Session, i *discordgo.InteractionCreate)
 }
 
 func (d *DiscordHandler) RegisterCommands() error {
 
-    customCommands := []DiscordCommand{
-        CreateMuteCommand(),
-    }
+	customCommands := []DiscordCommand{
+		CreateMuteCommand(),
+	}
 
-
-    // convert the custom commands into discordgo commands 
+	// convert the custom commands into discordgo commands
 	commands := make([]*discordgo.ApplicationCommand, 0)
-    commandHandlers := map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){}
-    for _, v := range customCommands {
-        commands = append(commands, v.CreateCommand())
-        commandHandlers[v.Name()] = v.HandleCommand
-    }
-
+	commandHandlers := map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){}
+	for _, v := range customCommands {
+		commands = append(commands, v.CreateCommand())
+		commandHandlers[v.Name()] = v.HandleCommand
+	}
 
 	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
 	for i, v := range commands {
@@ -38,14 +36,14 @@ func (d *DiscordHandler) RegisterCommands() error {
 			panic(err)
 		}
 		registeredCommands[i] = cmd
+		slog.Info(fmt.Sprintf("registered command %s", cmd.Name))
 	}
 
 	d.commands = registeredCommands
 
-
-    // adding stuff to actually handle the commands
-    d.session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-        slog.Info(fmt.Sprintf("got interaction: %s", i.ApplicationCommandData().Name))
+	// adding stuff to actually handle the commands
+	d.session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		slog.Info(fmt.Sprintf("got interaction: %s", i.ApplicationCommandData().Name))
 		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
 			h(s, i)
 		}
@@ -58,7 +56,8 @@ func (d *DiscordHandler) unregisterCommands() {
 	for _, v := range d.commands {
 		err := d.session.ApplicationCommandDelete(d.session.State.User.ID, utils.DiscordGuildId(), v.ID)
 		if err != nil {
-            slog.Error("failed to unregister command", err)
+			slog.Error("failed to unregister command", err)
 		}
+		slog.Info(fmt.Sprintf("unregistered command %s", v.Name))
 	}
 }
