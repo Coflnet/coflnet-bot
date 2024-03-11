76df511e47fef2e00313a5ee0b3e2951a58df425
Florian Muehlhans <muehlhans.f@coflnet.com>
2024-03-11 15:57:26 +0100

    add some debug info

--- internal/usecase/mute_command.go
+++ internal/usecase/mute_command.go
@@ -92,6 +92,8 @@ func (m *MuteCommand) Execute(s *discordgo.Session, i *discordgo.InteractionCrea
 		return
 	}
 
+	span.AddEvent("User allowed to mute")
+
 	username := opt.StringValue()
 	span.SetAttributes(attribute.String("username", username))
 	message := messageOpt.StringValue()
@@ -114,6 +116,7 @@ func (m *MuteCommand) Execute(s *discordgo.Session, i *discordgo.InteractionCrea
 	}
 
 	muter := i.Member.User.Username
+	span.SetAttributes(attribute.String("muter", muter))
 
 	// Mute user
 	err = m.muteUser(ctx, uuid, message, muter)
@@ -131,11 +134,28 @@ func (m *MuteCommand) Execute(s *discordgo.Session, i *discordgo.InteractionCrea
 			slog.Warn("Cannot respond to interaction", "err", err)
 		}
 	}
+
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: fmt.Sprintf("User %s muted", username),
+		}})
+
+	if err != nil {
+		span.RecordError(err)
+		slog.Warn("Cannot respond to interaction", "err", err)
+	}
+
+	span.AddEvent("User muted")
 }
 
 func (m *MuteCommand) muteUser(ctx context.Context, uuid, message, muter string) error {
 	ctx, span := m.tracer.Start(ctx, "mute-user")
 	defer span.End()
+
+	span.SetAttributes(attribute.String("uuid", uuid))
+	span.SetAttributes(attribute.String("message", message))
+	span.SetAttributes(attribute.String("muter", muter))
 
 	body := chatgen.PostApiChatMuteJSONRequestBody{
 		Message: strPtr(message),
@@ -147,14 +167,23 @@ func (m *MuteCommand) muteUser(ctx context.Context, uuid, message, muter string)
 		Authorization: strPtr(authorization()),
 	}
 
-	_, err := m.chatClient.PostApiChatMuteWithApplicationWildcardPlusJSONBody(ctx, &params, body)
+	response, err := m.chatClient.PostApiChatMuteWithApplicationWildcardPlusJSONBody(ctx, &params, body)
 	if err != nil {
 		span.RecordError(err)
 		slog.Error("Cannot mute user", "err", err)
 		return err
 	}
 
-	return nil
+	span.SetAttributes(attribute.Int("status_code", response.StatusCode))
+
+	if response.StatusCode >= 200 && response.StatusCode < 300 {
+		span.AddEvent("User muted")
+		return nil
+
+	}
+
+	span.AddEvent("Cannot mute user")
+	return fmt.Errorf("cannot mute user, status code: %d", response.StatusCode)
 }
 
 func userAllowedToMute(member *discordgo.Member) bool {
